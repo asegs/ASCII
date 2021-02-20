b4ff3eabbe171ebb6a5fcbf34ed8681761d6c0bd
jsl4627 <55163035+jsl4627@users.noreply.github.com>
2021-02-20 18:37:47 -0500

    Code update

--- pngReader/src/Reader.java
+++ pngReader/src/Reader.java
@@ -13,12 +13,8 @@ public class Reader {
         double[][][] imgData = new double[raster.getHeight()][raster.getWidth()][4];
         for (int row = 0; row < raster.getHeight(); row++) {
             for (int col = 0; col < raster.getWidth(); col++) {
-                raster.getPixel(col, row, imgData[row][col]);
-                double average = 0.0;
-                for (int i=0;i<3;i++){
-                    average+=imgData[row][col][i];
-                }
-                average = average/3;
+                //raster.getPixel(col, row, imgData[row][col]);
+                double average = (imgData[row][col][0] + imgData[row][col][1] + imgData[row][col][2]) / 3;
                 for (int i=0;i<3;i++){
                     imgData[row][col][i] = utils.boostContrastHex(imgData[row][col][i],contrastBoost,average);
                 }
