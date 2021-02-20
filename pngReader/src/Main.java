6106516b82a3f7d97165d39476eb6ae8741fc44a
ajs2290 <ajs2290@g.rit.edu>
2021-02-20 15:58:40 -0500

    Updated

--- pngReader/src/Main.java
+++ pngReader/src/Main.java
@@ -9,15 +9,15 @@ public class Main {
         Photo photo = reader.loadPhoto("C:\\Users\\aarse\\Documents\\pngReader\\src\\slight6.png",1.5);
         long startTime = System.nanoTime();
         Photo[][] grid = photo.toChunks(600, 180);
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int row=0;row<grid.length;row++){
             for (int col=0;col<grid[0].length;col++){
-                result+=toAscii.photoToAscii(grid[row][col]);
+                result.append(toAscii.photoToAscii(grid[row][col]));
             }
-            result+='\n';
+            result.append('\n');
         }
         long endTime = System.nanoTime();
         System.out.println(((endTime-startTime)/1000000)+"ms total run time.");
-        writer.write("render.txt",result);
+        writer.write("render.txt", result.toString());
     }
 }
