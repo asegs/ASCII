08ab73d38e24a6afcf532c7a6735259803f1b82c
Aaron <54863852+asegs@users.noreply.github.com>
2021-02-20 20:40:49 -0500

    Update Reader.java

--- pngReader/src/Reader.java
+++ pngReader/src/Reader.java
@@ -28,10 +28,6 @@ public class Reader {
        try {
             BufferedImage bufferedImage = ImageIO.read(new File(inputFile));
             ImageIO.write(bufferedImage, "png", new File(outputFile));
-            ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
-            ImageIO.write(bufferedImage, "png", byteArrayOut);
-            byte[] resultingBytes = byteArrayOut.toByteArray();
-
         } catch (IOException e) {
             System.out.println(e);
         }
